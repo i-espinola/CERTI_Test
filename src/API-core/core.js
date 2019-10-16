3a659360f3c3a908fe297243c1393cd3b839fe4d
i.espinola <ivan.espinola@live.com>
2019-10-15 21:47:41 -0300

    setttings-lint

--- src/API-core/core.js
+++ src/API-core/core.js
@@ -1,23 +1,19 @@
-const toWords = require("extenso");
+﻿'use ';
+
+const toWords = require('extenso');
 
 function validation(input) {
   const value = parseInt(input);
   const filter = {
-    min: value >= -99999 ? true : false,
-    max: value <= 99999 ? true : false,
+    min: value >= -99999,
+    max: value <= 99999,
     nan: !isNaN(value),
     finite: isFinite(value),
-    size: input.replace("-", "")
-      .length <= 5 ? true : false
-  }
+    size: input.replace('-', '')
+      .length <= 5,
+  };
 
-  const result = !!(
-    filter.min &&
-    filter.max &&
-    filter.nan &&
-    filter.finite &&
-    filter.size
-  );
+  const result = filter.min && filter.max && filter.nan && filter.finite && filter.size;
   return result;
 }
 
@@ -25,27 +21,27 @@ function startup(input) {
   const value = input.toString();
   const validate = validation(value);
 
-  if (input === "check") {
+  if (input === 'check') {
     return true;
-  } else if (validate) {
-    const isZero = { extenso: "zero" };
-    const runWebService = { extenso: toWords(value, { negative: "informal" }) };
-    const output = value === "0" ? isZero : runWebService;
+  }
+  if (validate) {
+    const isZero = { extenso: 'zero' };
+    const runWebService = { extenso: toWords(value, { negative: 'informal' }) };
+    const output = value === '0' ? isZero : runWebService;
 
     return JSON.stringify(output);
-  } else {
-    const errorObj = {
-      error: {
-        code: 400,
-        status: "error",
-        info: "valor inválido",
-        details: "entre com um valor numérico entre -99999 até 99999"
-      }
-    };
-    return JSON.stringify(errorObj);
   }
+  const errorObj = {
+    error: {
+      code: 400,
+      status: 'error',
+      info: 'valor inválido',
+      details: 'entre com um valor numérico entre -99999 até 99999',
+    },
+  };
+  return JSON.stringify(errorObj);
 }
 
 module.exports = {
-  startup: startup
+  startup,
 };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 // Requires modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const webService = require("./API-core/core");
+const express = require('express');
+const bodyParser = require('body-parser');
+const webService = require('./API-core/core');
 
 // Server configs
 const app = express();
-const input = "/:input";
-const port = 8000;
+const input = '/:input';
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 
 app.get(input, (request, response) => {
-  const output = request.params.input ?
-    webService.startup(request.params.input) :
-    null;
-
+  const output = request.params.input ? webService.startup(request.params.input) : null;
   response.send(output);
 });
 
 app.listen(port, () => {
-  console.log('Server on');
+  // eslint-disable-next-line no-console
+  console.info('Server on');
   return true;
-})
+});
