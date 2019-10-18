952060f4d6fdc6d1c48805c504b9e38c4e276064
i.espinola <ivan.espinola@live.com>
2019-10-18 14:59:47 -0300

    server on check and docker check

---
+++ src/api_core/core.js
@@ -0,0 +1,30 @@
+﻿// const input = '456';
+
+const toWords = require('extenso');
+const Validation = require('./validation');
+
+function startup(input) {
+  const value = input.toString();
+  const validate = Validation.check(value);
+
+  if (validate) {
+    // const isZero = { extenso: 'zero' };
+    const runWebService = { extenso: toWords(value, { negative: 'informal' }) };
+    const output = value === '0' ? { extenso: 'zero' } : runWebService;
+
+    return JSON.stringify(output);
+  } else {
+    const errorObj = {
+      error: {
+        code: 400,
+        status: 'error',
+        info: 'valor inválido',
+        details: 'entre com um valor numérico entre -99999 até 99999',
+      },
+    };
+    return JSON.stringify(errorObj);
+  }
+}
+module.exports = {
+  startup,
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
+/* eslint-disable no-console */
 // Requires modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const webService = require('./API-core/core');
 
 // Server configs
+const config = {
+  name: 'API Rest number to extenso',
+  banner: 'Server on',
+  input: '/:input',
+  port: parseInt(process.env.PORT, 10) || 8000,
+  host: process.env.HOST || 'localhost',
+};
+
 const app = express();
-const input = '/:input';
-const port = process.env.PORT || 8000;
-
 app.use(bodyParser.json());
-
-app.get(input, (request, response) => {
-  const output = request.params.input ? webService.startup(request.params.input) : null;
-  response.send(output);
+app.get(config.input, (request, response) => {
+  const output = request.params.input
+    ? 'OK' // ? webService.startup(request.params.input)
+    : null;
+  response.status(200).send(output);
 });
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.info('Server on');
-  return true;
+// @ts-ignore
+app.listen(config.port, config.host, (e) => {
+  if (e) {
+    throw new Error('Internal Server Error');
+  }
+  console.info('server on ');
+  console.info(`http://${config.host}:${config.port}`);
 });
