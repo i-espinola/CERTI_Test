a13fded7dac61d0dc9dd36bbe0ae28089a354cb6
i.espinola <ivan.espinola@live.com>
2019-10-21 19:00:01 -0300

    refator-base

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-/* eslint-disable no-console */
 // Requires modules
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// Server configs
-const config = {
-  name: 'API Rest number to extenso',
-  banner: 'Server on',
-  input: '/:input',
-  port: parseInt(process.env.PORT, 10) || 8000,
-  host: process.env.HOST || 'localhost',
+const extenso = require('./commonjs/white');
+const app = express();
+const server = {
+  settings: {
+    banner: 'settings on',
+    input: '/:input',
+    port: parseInt(process.env.PORT, 10) || 8000,
+    host: process.env.HOST || 'localhost',
+  },
+  error: {
+    code: 400,
+    status: 'Número/valor inválido',
+    details: 'entre com um valor numérico entre -99999 até 99999'
+  },
+  apiRun: function(input) {
+    // extenso(input, { negative: 'informal' })
+    return extenso(input)
+  }
 };
 
-const app = express();
 app.use(bodyParser.json());
-app.get(config.input, (request, response) => {
-  const output = request.params.input
-    ? 'OK' // ? webService.startup(request.params.input)
-    : null;
+app.get(server.settings.input, (request, response) => {
+  const output = isNaN(Number.parseInt(request.params.input))
+    ? server.error
+    : server.apiRun(request.params.input)
   response.status(200).send(output);
 });
 
-// @ts-ignore
-app.listen(config.port, config.host, (e) => {
-  if (e) {
-    throw new Error('Internal Server Error');
-  }
-  console.info('server on ');
-  console.info(`http://${config.host}:${config.port}`);
+app.listen(server.settings.port, server.settings.host, function()
+{
+  console.info('settings on ');
+  console.info(`http://${server.settings.host}:${server.settings.port}`);
 });
